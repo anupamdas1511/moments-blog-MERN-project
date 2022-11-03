60203a7cad25fd3d819f949ec3cae9a9e251e9e4
Anupam Das <escapevel11.2@gmail.com>
2022-11-03 15:16:31 +0530

    created Navbar component and styles for it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { Container, Grow, Grid } from '@material-ui/core'
 import { useDispatch } from 'react-redux'
 import { getPosts } from './actions/posts'
 
@@ -8,6 +8,7 @@ import Form from './Components/Form/Form'
 import useStyles from './styles'
 
 import memories from './images/memories.png'
+import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
     const classes = useStyles()
@@ -20,10 +21,7 @@ const App = () => {
 
     return (
         <Container maxWidth='lg'>
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height={60}/>
-            </AppBar>
+            <Navbar />
             <Grow in>
                 <Container>
                     <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
