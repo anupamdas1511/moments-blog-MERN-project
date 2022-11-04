121c1fd8f5fd282e4c0df9d5b10b06268f689742
Anupam Das <escapevel11.2@gmail.com>
2022-11-04 11:30:53 +0530

    Updated the Home page and Created Authentication page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid } from '@material-ui/core'
-import { useDispatch } from 'react-redux'
-import { getPosts } from './actions/posts'
-
-import Posts from './Components/Posts/Posts'
-import Form from './Components/Form/Form'
-import useStyles from './styles'
-
-import memories from './images/memories.png'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Container } from '@material-ui/core'
 import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+import Auth from "./Components/Auth/Auth";
 
 const App = () => {
-    const classes = useStyles()
-    const dispatch = useDispatch()
-    const [ currentId, setCurrentId ] = useState(null)
-
-    useEffect(() => {
-        dispatch(getPosts())
-    }, [currentId, dispatch])
-
     return (
-        <Container maxWidth='lg'>
-            <Navbar />
-            <Grow in>
-                <Container>
-                    <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId} />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
+        <BrowserRouter>
+            <Container maxWidth='lg'>
+                <Navbar />
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/auth' exact component={Auth} />
+                </Switch>
+            </Container>
+        </BrowserRouter>
     )
 }
 
