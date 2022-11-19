e56fdd280e28be0c8c61e2b9944f8433047393fd
Anupam Das <dasanupam151101@gmail.com>
2022-11-19 12:24:53 +0530

    Commited

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,13 +2,15 @@ import express from 'express'
 
 import { getPost, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
 
+import Auth from '../middlewares/auth.js'
+
 const router = express.Router()
 
 // Creating routes
 router.get('/', getPost)
-router.post('/', createPost)
-router.patch('/:id', updatePost)
-router.delete('/:id', deletePost)
-router.patch('/:id/likePost', likePost)
+router.post('/', Auth, createPost)
+router.patch('/:id', Auth, updatePost)
+router.delete('/:id', Auth, deletePost)
+router.patch('/:id/likePost', Auth, likePost)
 
 export default router
